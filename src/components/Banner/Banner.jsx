0a1080296b770be5ffd6239492a992c05442d8ce
MatheusGorga <matheusgobar@gmail.com>
2022-12-13 11:55:06 -0300

    component Banner

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,10 @@
+import './Banner.css';
+function Banner() {
+  return (
+    <header className='banner'>
+      <img src='/assets/banner.png' alt='Banner do organo' />
+    </header>
+  );
+}
+
+export default Banner;
