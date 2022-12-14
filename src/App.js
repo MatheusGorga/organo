2ff74b5c2328c229c97c514865206a8801ee2aa6
MatheusGorga <matheusgobar@gmail.com>
2022-12-13 22:40:29 -0300

    campoTexto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Banner from './components/Banner/Banner';
+import CampoTexto from './components/CampoTexto/CampoTexto';
 
 function App() {
   return (
     <div >
       <header >
         <Banner />
+        <CampoTexto label="Nome" />
       </header>
     </div>
   );
